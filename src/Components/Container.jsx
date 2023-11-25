8be48163fc9ff7a48de0b90cf8a7bb63c7af0227
wolfiee42 <saifalislam2022@gmail.com>
2023-11-25 22:54:44 +0600

    container component created

---
+++ src/Components/Container.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+
+
+const Container = ({children}) => {
+    return (
+        <div className="max-w-7xl mx-auto"> 
+            {children}
+        </div>
+    );
+};
+
+export default Container;
